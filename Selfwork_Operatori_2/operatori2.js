6043fbd044c5cca59214544391ddaa79b31a4da5
LisviaIlvia <silviaercolino@yahoo.it>
2024-07-26 20:12:20 +0200

    Add files via upload

---
+++ Selfwork_Operatori_2/operatori2.js
@@ -0,0 +1,8 @@
+let annoCorrente = 2024;
+let annoNascita = 1991;
+
+let età = annoCorrente - annoNascita;
+
+let secolo = 100 - età;
+
+alert('Hai ' + età + ' anni e ti mancano ' + secolo + ' anni per compierne 100!')
